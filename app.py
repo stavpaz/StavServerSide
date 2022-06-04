5824780759e0b77f6b5a84743d94afae255d5df4
stavpaz <stavpaz@post.bgu.ac.il>
2022-06-04 15:53:20 +0300

    url

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import  Flask,redirect,url_for
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return redirect(url_for('Home_page'))
+
+@app.route('/home.html')
+def Home_page():
+    return render_template('home.html', user_name='stav', user_second='cohen')
+
+@app.route('/aboutus.html')
+def AboutUs_page():
+    return render_template('aboutus.html', user_name='stav', user_second='cohen')
+
+@app.route('/contactus.html')
+def ContactUs_page():
+    return render_template('contactus.html', user_name='stav', user_second='cohen')
+@app.route('/design.html')
+def Design_page():
+    return render_template('design.html')
+
+
+@app.route('/assignment3_1.html')
+def Assign_page():
+    user_info = {'FirstName': 'Stav', 'SecondName': 'Paz', 'LastName': 'Cohen'}
+    degrees = ['Industrial Engineering', 'Mechanical engineering', 'management']
+    hobbies = ('Dance', 'TV', 'Sleep')
+
+    return render_template('assignment3_1.html', user_info=user_info, user_degrees=degrees, hobbies=hobbies)
+
+
+
+
+@app.route('/openyourmind.html')
+def OpenYourMind_page():
+    return render_template('openyourmind.html', user_name='stav', user_second='cohen')
+
+var=23
+
+@app.route('/test')
+def test_page():
+    return f'welcome ,{var}'
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
---
+++ static/scripts.js
@@ -0,0 +1,81 @@
+/*All pages -current page */
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+if(link.href.includes(`${activePage}`)){
+link.classList.add('active');
+}
+});
+
+/* Home page - slider photos */
+var counter=1;
+function SlidePhotos  (){
+    document.getElementById('bt'+counter).checked=true;
+    counter++;
+    if(counter>4)
+    {
+        counter=1;
+
+    }
+}
+setInterval(SlidePhotos,4000)
+
+/* Design page - loading images and animation */
+
+var slide=document.querySelectorAll('.Designslide');
+    var btn=document.querySelector('.btn');
+    var currenttime=4;
+    var count=1;
+
+
+    function anim(){
+
+        stop();
+        start();
+
+
+
+        for(var i=currenttime; i<currenttime+4; i++)
+        {
+
+            if(slide[i])
+            {
+                slide[i].style.display='block';
+            }
+
+
+        }
+
+        currenttime+=4;
+
+        if(currenttime>=slide.length)
+        {
+            document.getElementById("bt").style.display='none';
+            document.getElementById("dott1").style.display='none';
+            document.getElementById("dott2").style.display='none';
+            document.getElementById("dott3").style.display='none';
+
+
+        }
+
+        setTimeout(stop,989);
+
+
+
+
+    }
+
+    function stop()
+    {
+
+
+        document.getElementById("dott1").style.animationPlayState='paused';
+        document.getElementById("dott2").style.animationPlayState='paused';
+        document.getElementById("dott3").style.animationPlayState='paused';
+    }
+
+    function start()
+    {
+        document.getElementById("dott1").style.animationPlayState='running';
+        document.getElementById("dott2").style.animationPlayState='running';
+        document.getElementById("dott3").style.animationPlayState='running';
+    }

5661ff8fd5c0a330f934ac41051941d125d2fe43
stavpaz <stavpaz@post.bgu.ac.il>
2022-06-22 17:14:46 +0300

    this is the update4 for assignment 4

--- app.py
+++ app.py
@@ -194,75 +194,14 @@ def logout_func():
 def session_func():
     # print(session['CHECK'])
     return jsonify(dict(session))
-@app.route('/assignment4.html')
-def Assign4_page():
-    return render_template('assignment4.html')
-def interact_db(query, query_type: str):
-    return_value = False
-    connection = mysql.connector.connect(host='localhost',
-                                         user='root',
-                                         passwd='',
-                                         database='myflaskappdb')
-    cursor = connection.cursor(named_tuple=True)
-    cursor.execute(query)
-    #
+# @app.route('/assignment4.html')
+# def Assign4_page():
+#     return render_template('assignment4.html')
+from pages.assignment4.assignment4 import assignment4
+app.register_blueprint(assignment4)
 
-    if query_type == 'commit':
-        # Use for INSERT, UPDATE, DELETE statements.
-        # Returns: The number of rows affected by the query (a non-negative int).
-        connection.commit()
-        return_value = True
 
-    if query_type == 'fetch':
-        # Use for SELECT statement.
-        # Returns: False if the query failed, or the result of the query if it succeeded.
-        query_result = cursor.fetchall()
-        return_value = query_result
 
-    connection.close()
-    cursor.close()
-    return return_value
-
-@app.route('/users')
-def users():
-    query = 'select * from users'
-    users_list = interact_db(query, query_type='fetch')
-    return render_template('users.html', users=users_list)
-
-@app.route('/insert_user', methods=['POST'])
-def insert_user():
-    name = request.form['inputname']
-    username = request.form['inputUsername']
-    email = request.form['inputEmail']
-    password = request.form['inputPassword']
-    #print(f'{name} {email} {password}')
-    query = "INSERT INTO users(username,name, email, password) VALUES ('%s','%s', '%s', '%s')" % (username,name, email, password)
-    interact_db(query=query, query_type='commit')
-    return redirect('/users')
-
-@app.route('/updateuser', methods=['POST'])
-def up_user():
-    username = request.form['inputusername']
-    name = request.form['inputname']
-    email = request.form['inputEmail']
-    password = request.form['inputPassword']
-    query = "UPDATE users SET name ='%s',email ='%s',password='%s'WHERE username='%s';" % (name, email, password,username)
-    interact_db(query=query, query_type='commit')
-    return redirect('/users')
-
-@app.route('/update_user', methods=['GET','POST'])
-def update_user():
-    user_id = request.form['user_id']
-    userToUdate=user_id
-    return render_template('updateusers.html',username=user_id)
-
-@app.route('/delete_user', methods=['POST'])
-def delete_user_func():
-    user_id = request.form['user_id']
-    query = "DELETE FROM users WHERE username='%s';" % user_id
-    # print(query)
-    interact_db(query, query_type='commit')
-    return redirect('/users')
 
 if __name__ == '__main__':
     app.run(debug=True)
